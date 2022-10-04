6acb578c6858df36bbb200455c56519e12f40049
malikamar104700 <malikamar104800@gmail.com>
2022-10-04 10:14:27 +0500

    string practice

---
+++ app.js
@@ -0,0 +1,30 @@
+console.log("Assignment 07");
+//=> Program # 1:
+// var a:string = "Amar";
+// var b:any = a.length
+// console.log(b);
+//Program # 3:
+// var a:string = "Amar"
+// var b:string = "Rehman"
+// console.log(a.concat(b))
+//=>Program # 4:
+// var a:string = "amar"
+// var b:string = "moeen"
+// var result:any  = a.localeCompare(b);
+// if(result == 0){
+// console.log("The strings are compare");
+// }else{
+//     console.log("The strings are not compare");
+// }
+//=> program # 5:
+// var a:string = "hello";
+// console.log("the string is lower case:", a);
+// var b:any = a.toUpperCase()
+// console.log("String convert Lowercase o Uppercase:",b);
+//=>Program # 6:
+// var a:string = "HELLO"
+// var b:any= a.toLowerCase()
+// console.log(b);
+var a = "I love Pakistan";
+var b = a.split(" ");
+console.log(b.length);
